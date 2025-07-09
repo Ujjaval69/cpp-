8cbf453524cf2a3da8d7113020ef2a34cea09850
Ujjaval Goyal <ujjavalgoyal5@gmail.com>
2025-07-09 14:27:47 +0530

    initial commit

---
+++ 2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    cin>>a;
+    cin>>b;
+    int temp;
+    temp=b;
+    b=a;
+    a=temp;
+    cout<<"a is:"<<a<<endl;
+    cout<<"b is:"<<b;
+
+
+
+    
+}
---
+++ 3.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=15;
+    int b=3;
+    cout<<a+b<<endl;
+    cout<<a-b<<endl;
+    cout<<a*b<<endl;
+    cout<<a/b<<endl;
+}
