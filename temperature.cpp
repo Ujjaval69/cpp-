83d6fd657726fb1953ccdef12cb984d023c4ba2a
Ujjaval Goyal <ujjavalgoyal5@gmail.com>
2025-07-23 15:07:52 +0530

    commited

--- temperature.cpp
+++ temperature.cpp
@@ -1,14 +1,19 @@
 #include<iostream>
 using namespace std;
 int main(){
-    float a;
-    char b[100];
-    cout<<"Enter temperature"<<endl;
-    cin>>a;
-    cout<<"Enter unit"<<endl;
-    cin>>b;
-    if(b=='C'){
-
+    double temp,unit,celsius,fah,kelvin;
+    cout<<"Enter temperature:"<<endl;
+    cin>>temp;
+    cout<<"Enter unit:"<<endl;
+    cin>>unit;
+    if (unit=='C'){
+        celsius=temp;
+    }else if (unit=='F'){
+        celsius=((temp-32)*5)/9;
+    }else if (unit=='K'){
+        celsius=temp-273.15;
+    }else{
+        cout<<"Invalid input";
     }
     
 }
