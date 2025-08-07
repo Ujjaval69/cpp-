b1f168d02514caabe228fea1609362fd489d3da6
Ujjaval Goyal <ujjavalgoyal5@gmail.com>
2025-08-07 11:34:39 +0530

    ujjaval

--- arrayreverse.cpp
+++ arrayreverse.cpp
@@ -1,18 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a;
-    cin>>a;
-    int arr[a];
-    for (int i=0;i<a;i++){
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i=0;i<n;i++){
         cin>>arr[i];
     }
-    for (int i=a-1;i>=0;i--){
-        cout<<arr[i];
+    for (int i=n-1;i>=0;i--){
+        cout<<arr[i]<<endl;
     }
-
-    
-   
-
-
 }
--- functions.cpp
+++ functions.cpp
@@ -1,22 +1,39 @@
+// #include<iostream>
+// using namespace std;
+// int fact(int n){
+//     int p=1;
+//     for (int i=1;i<=n;i++){
+//         p=p*i;
+//     }
+//     return p;
+// }
+// int ncr(int n,int r){
+//     int fact_n=fact(n);
+//     int fact_r=fact(r);
+//     int fact_nmr=fact(n-r);
+//     return fact(n)/(fact(r)*fact(n-r));
+
+// }
+// int main(){
+//     int n,r;
+//     cin>>n;
+//     cin>>r;
+//     cout<<ncr(n,r);
+// }
 #include<iostream>
 using namespace std;
-int fact(int n){
+void Fact(int n){
     int p=1;
     for (int i=1;i<=n;i++){
         p=p*i;
-    }
-    return p;
-}
-int ncr(int n,int r){
-    int fact_n=fact(n);
-    int fact_r=fact(r);
-    int fact_nmr=fact(n-r);
-    return fact(n)/(fact(r)*fact(n-r));
 
+    }
+    cout<<p;
 }
 int main(){
-    int n,r;
+    int n;
     cin>>n;
-    cin>>r;
-    cout<<ncr(n,r);
+    Fact(n);
+
+
 }
